948addf465eb732fd71fb5e76b389658d0d90fcb
Amy <asentientbot@gmail.com>
2024-07-04 01:34:45 -0400

    organize interposer build script again
    last time, i swear

--- sequoia 31001 interposer/build.zsh
+++ sequoia 31001 interposer/build.zsh
@@ -1,12 +1,11 @@
 set -e
 
-victim="$1"
-cp "${victim}_original" "$victim"
-
+driverName="$1"
 codeName=interposer.m
-impersonated=/System/Library/Frameworks/IOSurface.framework/Versions/A/IOSurface
 impostorName=impostor.dylib
+impersonatedPath=/System/Library/Frameworks/IOSurface.framework/Versions/A/IOSurface
 
-impostorPath="/System/Library/Extensions/$victim.bundle/Contents/MacOS/$impostorName"
-install_name_tool -change "$impersonated" "$impostorPath" "$victim"
-clang -fmodules -dynamiclib "$codeName" -Wno-unused-getter-return-value -Xlinker -no_warn_inits -I non-metal-common/Utils -I . -install_name "$impostorPath" -Xlinker -reexport_library -Xlinker "$impersonated" -o "$impostorName"
+cp "${driverName}_original" "$driverName"
+impostorPath="/System/Library/Extensions/$driverName.bundle/Contents/MacOS/$impostorName"
+install_name_tool -change "$impersonatedPath" "$impostorPath" "$driverName"
+clang -fmodules -dynamiclib "$codeName" -Wno-unused-getter-return-value -Xlinker -no_warn_inits -I non-metal-common/Utils -I . -install_name "$impostorPath" -Xlinker -reexport_library -Xlinker "$impersonatedPath" -o "$impostorName"
