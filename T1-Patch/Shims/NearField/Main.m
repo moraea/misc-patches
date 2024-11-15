21819000809d52a6ab2ca953eea9aa9ea8d2b4c8
Amy <asentientbot@gmail.com>
2024-11-14 23:03:00 -0500

    update NearField shim for apple pay
    Squashed commit of the following:
    
    commit 66f4e7467afd5476df51b506875d0a27c122f4ce
    Author: Amy <asentientbot@gmail.com>
    Date:   Thu Nov 14 22:53:07 2024 -0500
    
        add todos
    
        ..not that i'll ever fix them
    
        Co-Authored-By: covasedu <187997700+covasedu@users.noreply.github.com>
    
    commit c382470464f7b8f027dfe84fbdafe73cf0855f44
    Author: Amy <asentientbot@gmail.com>
    Date:   Thu Nov 14 22:49:59 2024 -0500
    
        remove excess logging
    
        apparently this works, yay
    
        Co-Authored-By: covasedu <187997700+covasedu@users.noreply.github.com>
    
    commit 938cc693037d07e8df37caec5c6f9ef892a1814e
    Author: Amy <asentientbot@gmail.com>
    Date:   Thu Nov 14 22:35:17 2024 -0500
    
        second try!
    
        Co-Authored-By: covasedu <187997700+covasedu@users.noreply.github.com>
    
    commit 31a4dea5cc432c9721a412fb942c3de429156441
    Author: Amy <asentientbot@gmail.com>
    Date:   Thu Nov 14 22:14:11 2024 -0500
    
        untested embeddedSecureElementSerialNumber shim
    
    Co-Authored-By: covasedu <187997700+covasedu@users.noreply.github.com>

--- T1-Patch/Shims/NearField/Main.m
+++ T1-Patch/Shims/NearField/Main.m
@@ -1,3 +1,5 @@
+#define trace NSLog
+
 @import Foundation;
 
 @interface NFSecureElement:NSObject
@@ -6,8 +8,24 @@
 @end
 @implementation NFSecureElement(Stub)
 
-+(id)embeddedSecureElementSerialNumber
++(NSData*)embeddedSecureElementSerialNumber
 {
-	return nil;
+	// TODO: this is the cache miss path and will probably be slow
+	// TODO: these functions generate a lot of warnings
+	
+	NSObject* manager=[NSClassFromString(@"NFHardwareManager") sharedHardwareManager];
+	
+	NSError* error=nil;
+	NSObject* element=[manager secureElementWithIdentifier:@"embedded" error:&error];
+	
+	return [NSData NF_dataWithHexString:[element serialNumber]];
 }
+
 @end
+
+// TODO: this was crashing as a bad bind, which is weird. we should've at least gotten a stub..
+
+NSString* NFSecureElementSerialNumberToString(NSData* data)
+{
+	return [data NF_asHexString];
+}
