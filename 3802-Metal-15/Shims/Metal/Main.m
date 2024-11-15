5b484bd85f7cd497ac2c8d25e449f1c12ff428d6
covasedu <eduardocovas@outlook.com>
2024-11-14 21:17:22 -0300

    update metal 3802 shims

--- 3802-Metal-15/Shims/Metal/Main.m
+++ 3802-Metal-15/Shims/Metal/Main.m
@@ -1,5 +1,15 @@
 #import "Utils.h"
 
+void add(Class class,NSString* selString,IMP imp,char* types)
+{
+	trace(@"cw: class_addMethod %@ %d",selString,class_addMethod(class,NSSelectorFromString(selString),imp,types));
+}
+
+id fake(id self,SEL sel,id rdx)
+{
+	return nil;
+}
+
 long fake_true()
 {
 	return 1;
@@ -21,16 +31,25 @@ void load()
 
 		swizzleImp(@"MTLCompiler",@"validateLanguageAndAIRVersionForFunction:completionHandler:",true,(IMP)fake_true,NULL);
 		
-		if([NSProcessInfo.processInfo.arguments[0] containsString:@"WallpaperSequoiaExtension"])
-		{
-        	swizzleImp(@"MTLTextureDescriptorInternal",@"validateWithDevice:",true,(IMP)fake_true,NULL);
-			swizzleImp(@"MTLRenderPipelineDescriptorInternal",@"validateWithDevice:error:",true,(IMP)fake_true,NULL);
-		}
+		// xcode crash
+		add(NSClassFromString(@"MTLHeapDescriptorInternal"),@"addressRanges",(IMP)fake,"@@:@");
+		add(NSClassFromString(@"MTLHeapDescriptorInternal"),@"setAddressRanges:",(IMP)fake,"@@:@");
 		
+		// WhatsApp app crash
+		add(NSClassFromString(@"NSProcessInfo"),@"isDeviceCertifiedFor:",(IMP)fake,"@@:@");
+		add(NSClassFromString(@"NSProcessInfo"),@"hasPerformanceProfile:",(IMP)fake,"@@:@");
+		
+		// hide distracting items
 		if([NSProcessInfo.processInfo.arguments[0] containsString:@"Safari"])
 		{
         	swizzleImp(@"_MTLFunction",@"newArgumentEncoderWithBufferIndex:reflection:functionReflection:",true,(IMP)fake_kil,NULL);
         	swizzleImp(@"_MTLDevice",@"newArgumentEncoderWithBufferBinding:",true,(IMP)fake_kil,NULL);
+		}
+		
+		if([NSProcessInfo.processInfo.arguments[0] containsString:@"WallpaperSequoiaExtension"])
+		{
+        	swizzleImp(@"MTLTextureDescriptorInternal",@"validateWithDevice:",true,(IMP)fake_true,NULL);
+			swizzleImp(@"MTLRenderPipelineDescriptorInternal",@"validateWithDevice:error:",true,(IMP)fake_true,NULL);
 		}
 	}
 }
