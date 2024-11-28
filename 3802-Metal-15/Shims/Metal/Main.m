481406685ecf88bad379b8fcba905a358fc233ec
Amy <asentientbot@gmail.com>
2024-11-27 20:36:23 -0500

    change add function to check swizzleLog
    we should put this in utils..?

--- 3802-Metal-15/Shims/Metal/Main.m
+++ 3802-Metal-15/Shims/Metal/Main.m
@@ -2,7 +2,11 @@
 
 void add(Class class,NSString* selString,IMP imp,char* types)
 {
-	trace(@"cw: class_addMethod %@ %d",selString,class_addMethod(class,NSSelectorFromString(selString),imp,types));
+	BOOL result=class_addMethod(class,NSSelectorFromString(selString),imp,types);
+	if(swizzleLog)
+	{
+		trace(@"add %@ %@ returned %d",NSStringFromClass(class),selString,result);
+	}
 }
 
 id fake(id self,SEL sel,id rdx)
@@ -19,16 +23,18 @@ long fake_kil()
 {
 	return 0;
 }
-	
+
 __attribute__((constructor))
 void load()
 {
 	@autoreleasepool
 	{
-		traceLog=false;
+		traceLog=true;
 		tracePrint=false;
 		swizzleLog=false;
-
+		
+		NSString* process=NSProcessInfo.processInfo.arguments[0];
+		
 		swizzleImp(@"MTLCompiler",@"validateLanguageAndAIRVersionForFunction:completionHandler:",true,(IMP)fake_true,NULL);
 		
 		// xcode crash
@@ -40,13 +46,13 @@ void load()
 		add(NSClassFromString(@"NSProcessInfo"),@"hasPerformanceProfile:",(IMP)fake,"@@:@");
 		
 		// hide distracting items
-		if([NSProcessInfo.processInfo.arguments[0] containsString:@"Safari"])
+		if([process containsString:@"Safari"])
 		{
         	swizzleImp(@"_MTLFunction",@"newArgumentEncoderWithBufferIndex:reflection:functionReflection:",true,(IMP)fake_kil,NULL);
         	swizzleImp(@"_MTLDevice",@"newArgumentEncoderWithBufferBinding:",true,(IMP)fake_kil,NULL);
 		}
 		
-		if([NSProcessInfo.processInfo.arguments[0] containsString:@"WallpaperSequoiaExtension"])
+		if([process containsString:@"WallpaperSequoiaExtension"])
 		{
         	swizzleImp(@"MTLTextureDescriptorInternal",@"validateWithDevice:",true,(IMP)fake_true,NULL);
 			swizzleImp(@"MTLRenderPipelineDescriptorInternal",@"validateWithDevice:error:",true,(IMP)fake_true,NULL);
