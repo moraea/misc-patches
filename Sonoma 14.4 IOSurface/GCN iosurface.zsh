d691dd7864baefca34c16589f0facc8cdd6750b4
Amy <asentientbot@gmail.com>
2025-06-09 01:58:12 -0400

    mess up edu's gcn fb patcher
    no functional change, don't push

--- Sonoma 14.4 IOSurface/GCN iosurface.zsh
+++ Sonoma 14.4 IOSurface/GCN iosurface.zsh
@@ -1,42 +1,54 @@
-./Binpatcher AMDRadeonX4000.kext/Contents/MacOS/AMDRadeonX4000 AMDRadeonX4000.kext/Contents/MacOS/AMDRadeonX4000 '
+set -e
+
+montereyPlaneCount=0xb0
+sequoiaPlaneCount=0x98
+patchPlaneCount="assert $montereyPlaneCount
+write $sequoiaPlaneCount"
+
+montereyPixelFormat=0xa4
+sequoiaPixelFormat=0x80
+patchPixelFormat="assert $montereyPixelFormat
+write $sequoiaPixelFormat"
+
+Binpatcher AMDRadeonX4000.kext/Contents/MacOS/AMDRadeonX4000 AMDRadeonX4000.kext/Contents/MacOS/AMDRadeonX4000 "
 
 # IOSurface::getPlaneCount
 
 # AMDRadeonX4000_AMDAccelResource::zeroVidMemory
 
 set 0x1493d
-write 0x98
+$patchPlaneCount
 
 # AMDRadeonX4000_AMDAccelResource::getIOSurfacePlaneInfo
 
 set 0x170da
-write 0x98
+$patchPlaneCount
 
 set 0x1712b
-write 0x98
+$patchPlaneCount
 
 # AMDRadeonX4000_AMDAccelResource::fillUBMSurfaceInfoBacking
 
 set 0x178ea
-write 0x98
+$patchPlaneCount
 
 # AMDRadeonX4000_AMDAccelResource::initIOSurface
 
 set 0x168e0
-write 0x98
+$patchPlaneCount
 
 # AMDRadeonX4000_AMDVIResource::initIOSurface
 
 set 0x5827c
-write 0x98
+$patchPlaneCount
 
 # IOSurface::getPixelFormat
 
 # AMDRadeonX4000_AMDAccelResource::zeroVidMemory (again)
 
 set 0x1494a
-write 0x80
+$patchPixelFormat
 
-'
+"
 
 codesign -fs - AMDRadeonX4000.kext/Contents/MacOS/AMDRadeonX4000
