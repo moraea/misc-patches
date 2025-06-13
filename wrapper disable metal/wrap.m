051fd343eeb997667a5efed206bf7d75306f203c
Amy <asentientbot@gmail.com>
2025-06-13 00:59:43 -0400

    restore separate metal is kil code
    keep the actually shipping @covasedu version than whatever i modified and didn't even end up using, oops

--- wrapper disable metal/wrap.m
+++ wrapper disable metal/wrap.m
@@ -1,10 +1,25 @@
 #import "Utils.h"
-#import "metaliskil.m"
 
-__attribute__((constructor))void load()
+id returnNil()
 {
-	tracePrint=false;
+	return nil;
+}
+
+__attribute__((constructor))void l()
+{
 	traceLog=true;
+	tracePrefix=@"Amy";
 	
-	metalIsKilSetup();
+	if([@[@"/System/Library/CoreServices/NotificationCenter.app/Contents/MacOS/NotificationCenter",@"/System/Applications/Font Book.app/Contents/MacOS/Font Book"] containsObject:NSProcessInfo.processInfo.arguments[0]])
+	{
+		if([NSUserDefaults.standardUserDefaults boolForKey:@"Amy.IvyMetalDisableDisable"])
+		{
+			trace(@"no lack of Metal for you!");
+		}
+		else
+		{
+			trace(@"no Metal for you!");
+			swizzleImp(@"MTLIGAccelDevice",@"initWithAcceleratorPort:",true,(IMP)returnNil,NULL);
+		}
+	}
 }
